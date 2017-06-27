2e21e5aff9780dc29657118e793c6c7e48cbaf1f
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-06-27 14:32:49 +0300

    1.7.2

--- sovrin_node/__metadata__.py
+++ sovrin_node/__metadata__.py
@@ -1,7 +1,7 @@
 """
 sovrin-node package metadata
 """
-__version_info__ = (1, 7, 1)
+__version_info__ = (1, 7, 2)
 __version__ = '.'.join(map(str, __version_info__))
 __author__ = "Sovrin Foundation."
 __license__ = "Apache 2.0"
